b52ff500400194933efb9ebd1abef27b2ae40b1e
Marko Cabrilo <tvoj@email.com>
2025-09-07 18:51:37 +0200

    Kreiran account model

--- seminarskiBudzetiranje-app/app/Models/Account.php
+++ seminarskiBudzetiranje-app/app/Models/Account.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-
+//acc
 class Account extends Model
 {
     protected $fillable = ['user_id', 'name', 'balance'];
