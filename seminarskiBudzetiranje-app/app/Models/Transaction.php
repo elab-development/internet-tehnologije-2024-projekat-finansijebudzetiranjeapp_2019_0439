1236b6fe2a3343da67b605d4814759eeb28787e0
Marko Cabrilo <tvoj@email.com>
2025-09-07 17:32:58 +0200

    Kreiran transaction model

--- seminarskiBudzetiranje-app/app/Models/Transaction.php
+++ seminarskiBudzetiranje-app/app/Models/Transaction.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Transaction extends Model
 {
-    //
+    protected $fillable = [
+        'account_id', 'category_id',
+        'amount', 'description',
+        'transaction_date'
+    ];
+
+    public function account(): BelongsTo
+    {
+        return $this->belongsTo(Account::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
