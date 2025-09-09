b8d623a9f7ad99bbc3b78b3ce74694e52afac013
Marko Cabrilo <tvoj@email.com>
2025-09-09 17:49:09 +0200

    Kreirana add unique migracija i zavrsen kompletan drugi zahtev

--- SeminarskiBudzetiranje-app/database/migrations/2025_09_09_154656_add_unique_user_name_to_categories_table.php
+++ SeminarskiBudzetiranje-app/database/migrations/2025_09_09_154656_add_unique_user_name_to_categories_table.php
@@ -4,25 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class AddUniqueUserNameToCategoriesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('categories', function (Blueprint $table) {
-            //
+            $table->unique(['user_id', 'name'], 'categories_user_name_unique');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('categories', function (Blueprint $table) {
-            //
+            $table->dropUnique('categories_user_name_unique');
         });
     }
-};
+}
