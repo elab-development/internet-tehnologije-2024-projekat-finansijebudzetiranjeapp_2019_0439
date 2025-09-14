5f7d7c53c4e577db17eab503ae6ca2f51534022d
Marko Cabrilo <tvoj@email.com>
2025-09-14 14:00:15 +0200

    Azuriranje App.jsx i odradjeni zahtevi za kreiranje komponenti i stilizovanje

--- SeminarskiBudzetiranje-app/resources/js/app.jsx
+++ SeminarskiBudzetiranje-app/resources/js/app.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import '../css/app.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Stranice
+import '../css/app.css';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Transactions from './pages/Transactions';
 
-// Glavna App komponenta
+// Komponente
+import Navbar from './components/Navbar';
+
 function App() {
     return (
         <Router>
-            <nav style={{ padding: '10px', background: '#f0f0f0' }}>
-                <Link to="/" style={{ marginRight: '10px' }}>Home</Link>
-                <Link to="/login" style={{ marginRight: '10px' }}>Login</Link>
-                <Link to="/transactions">Transactions</Link>
-            </nav>
+            <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
